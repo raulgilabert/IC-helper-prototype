1a616273c3392c48b53af00c4e9f8657e89a0238
raulgilabert <raulgilabertgamez@gmail.com>
2021-11-17 20:28:12 +0100

    Added conversion of codes 0000 and 0001

--- main.py
+++ main.py
@@ -1,20 +1,52 @@
 import custom_input
 import json
-import pprint
 
 
 def conversion_mnemonic_binary():
     data: dict = json.load(open("./data.json"))
-    switch_operations: dict = data.get("mnemonicToBinary")
-    switch_registers: dict = data.get("registers")
+    data_operations: dict = data.get("mnemonicToBinary")
+    data_registers: dict = data.get("registers")
 
     data_received: list = input("Mnemonic to convert to binary: \n").split(" ")
 
+    list_regs: list = []
+
+    # Add elements inputted from the user to a list splitting by the spaces
+    # and deleting the commas
+    index: int = 0
     for element in data_received:
-        element.replace(",", "")
+        data_received[index] = element.replace(",", "")
 
+        # Add element to the list if it is a register
+        if element[0] == "R":
+            list_regs.append(data_received[index])
 
+        index += 1
 
+    # get the data of the command inputted by the user
+    data_mnemonic: dict = data_operations.get(data_received[0])
+
+    counter_regs: int = 0
+    binary: str = ""
+
+    # Convert the input to binary
+    for element_to_print in data_mnemonic.get("dataInfo"):
+        if element_to_print[0] == "code":
+            binary += data_mnemonic.get("code")
+
+        elif element_to_print[0] == "reg":
+            binary += data_registers.get(list_regs[counter_regs])
+
+            counter_regs += 1
+
+        elif element_to_print[0] == "operation":
+            binary += data_mnemonic.get("operation")
+
+    print("binary: " + binary)
+
+    # Convert the string binary to an integer in base 2 and print it in
+    # hexadecimal
+    print("hexadecimal: {:04X}".format(int(binary, 2)))
 
 
 def conversion_binary_mnemonic():
